008e1cfbce9fe371c24240e432853a8d743cf21e
davemccann93 <davemccann93@dhcp-892b1621.ucd.ie>
2015-02-08 20:43:32 +0000

    Updated Player class

--- src/Player.java
+++ src/Player.java
@@ -3,24 +3,67 @@ import java.util.Scanner;
 import javax.swing.JOptionPane;
 
 public class Player {
+	
+	//variables of type frame
+	Frame playerFrame;
+	
+	private int playerScore;
+	private String PlayerName;		
 
-	public int playerScore = 0;
-	String PlayerName;		//
-	
-	public void setName(String n){		//Allows name of player to be set
-		PlayerName = n;
+	//allow access to frame
+	public void getPlayerFrame() {
+		playerFrame.printTiles();
 	}
 	
-	public String getName() {			//Allows display of player name
-		return PlayerName;
+	//allow access to frame
+	public void setPlayerFrame(Frame playerFrame) {
+		this.playerFrame = new Frame();
 	}
-	
-	//Returns players current score
-	public int getCurrentScore()
-	{
+
+	//allows access to player score
+	public int getPlayerScore() {
 		return playerScore;
 	}
 
+
+	public void setPlayerScore(int playerScore) {
+		this.playerScore = playerScore;
+	}
+
+
+	public String getPlayerName() {
+		return PlayerName;
+	}
+
+	//method to allow player name to be set
+	public void setPlayerName(String playerName) {
+		PlayerName = playerName;
+	}
+
+	//Constructor
+	public Player(){
+		this.PlayerName = "";
+		this.playerScore = 0;
+		this.playerFrame = new Frame();
+	}
+	
+	//Constructor
+	public Player(String name, int score, Frame frame){
+		this.PlayerName = name;
+		this.playerScore = score;
+		this.playerFrame = frame;
+	}
+	
+	public void setPlayer(){
+		Player player = new Player();
+		player.setPlayerFrame(playerFrame);
+	}
+
+	//method to allow player data to be reset
+	public Player resetPlayer(Player p){
+		p = new Player();
+		return null;	
+	}
 	
 	
 	//Adds points from users entered word to players score
@@ -28,16 +71,9 @@ public class Player {
 	{
 		int addedScore = 0;
 		
-		addedScore = getNewScore();
+		addedScore = getPlayerScore();
 		playerScore += addedScore;
 		System.out.println("Score after move is: " +addedScore+ " points.");
   
 	} 
-
-	//Returns players current score
-	public int getCurrentScore()
-	{
-		return playerScore;
-	}
-
 }
