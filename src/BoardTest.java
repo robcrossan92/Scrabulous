6b1db062173cfc76f9251a5404d1d7120e5b7a6d
davemccann93 <davemccann93@dhcp-892b1643.ucd.ie>
2015-02-18 13:36:29 +0000

    Updated test class

--- src/BoardTest.java
+++ src/BoardTest.java
@@ -1,3 +1,5 @@
+import javax.swing.JOptionPane;
+
 /**
  * Scrabulous
  * 
@@ -12,6 +14,25 @@
 public class BoardTest {
 
 	public static void main(String[] args) {
-
+		//int CENTRE = 7;
+		Board board = new Board();
+		Player Player1 = new Player("P1Name");
+		//Player Player2 = new Player("P2Name");
+		System.out.println(board);
+		System.out.println(Player1.getPlayerFrame());
+		String word = JOptionPane.showInputDialog(null, "Enter word:");
+		board.placeDown(7, 7, word, Player1);
+		System.out.println(board);
+		System.out.println(board.checkFirstWordOnCentre(word));
+		//System.out.println(board.checkFrameForWord(board.getPlayer1(), board.getPlayer1().enterWord(word)));
+		System.out.println(Player1.getPlayerFrame());
+		word = JOptionPane.showInputDialog(null, "Enter word:");
+		board.placeRight(8, 7, word, Player1);
+		//board.placeRight(2, 3, word, board.getPlayer1());
+		System.out.println(board);
+		System.out.println(Player1.getPlayerFrame());
+		
+		
+		
 	}
 }
