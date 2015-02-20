6fb6ce2b22df72de4f226def745fd01a56c0e463
NiamhK <niamh.kavanagh@ucdconnect.ie>
2015-02-20 12:39:44 +0000

    Complete board test

--- src/BoardTest.java
+++ src/BoardTest.java
@@ -14,27 +14,41 @@ import javax.swing.JOptionPane;
 public class BoardTest {
 
 	public static void main(String[] args) {
+		//Stores the current tile positions, stores the square values (e.g. triple word score)
 		Board board = new Board();
+		int row1 = 8, row2 = 14;
+		int col1 = 8, col2 = 14;
+		char dir1 = 'd', dir2 = 'R';
 		
-		Player Player1 = new Player("P1Name");
-		Player Player2 = new Player("P2Name");
+		Player player1 = new Player("P1Name");
+		Player player2 = new Player("P2Name");
 		
+		//Displays the current board using ASCII characters on the console
 		System.out.println(board);
-		System.out.println("Player 1:\n" + Player1.getPlayerFrame());
-		String word = JOptionPane.showInputDialog(null, "Enter word:");
-		System.out.println(board.isWordInFrame(Player1, word));
-		board.playWord(8, 8, word, Player1, 'd');
-		System.out.println(board);
-		System.out.println(board.isFirstWordOnCentre(word));
-		//System.out.println(board.checkFrameForWord(board.getPlayer1(), board.getPlayer1().enterWord(word)));
-		System.out.println(Player1.getPlayerFrame());
-		word = JOptionPane.showInputDialog(null, "Enter word:");
-		board.playWord(8, 8, word, Player1, 'r');
-		//board.placeRight(2, 3, word, board.getPlayer1());
-		System.out.println(board);
-		System.out.println(Player1.getPlayerFrame());
 		
+		//Player 1
+		System.out.println("Player 1:\n" + player1.getPlayerFrame());
+		String word1 = JOptionPane.showInputDialog(null, "Enter word:");
+		board.playWord(row1, col1, word1, player1, dir1);
+		System.out.println("Is word in frame: " + board.isWordInFrame(player1, word1));								//Expected true
+		System.out.println("Is first word on centre: " + board.isFirstWordOnCentre(word1));							//Expected true
+		System.out.println("Is word within bounds: " + board.isWordWithinBounds(row1, col1, word1));				//Expected true
+		System.out.println("Is word conflicting: " + board.isWordConflicting(row1, col1, word1, player1, dir1) + "\n");	//Expected true
+		System.out.println(board);
 		
+		//Player 2
+		System.out.println("Player 2:\n" + player2.getPlayerFrame());
+		String word2 = JOptionPane.showInputDialog(null, "Enter word:");
+		board.playWord(row2, col2, word2, player2, dir2);
+		System.out.println("Is word in frame: " + board.isWordInFrame(player2, word2));								//Expected false
+		System.out.println("Is first word on centre: " + board.isFirstWordOnCentre(word2));							//Expected false
+		System.out.println("Is word within bounds: " + board.isWordWithinBounds(row2, col2, word2));				//Expected false
+		System.out.println("Is word conflicting: " + board.isWordConflicting(row2, col2, word2, player2, dir2));	//Expected false
+		System.out.println(board);
+		
+		//Reset board
+		board.resetBoard();
+		System.out.println(board);
 		
 	}
 }
